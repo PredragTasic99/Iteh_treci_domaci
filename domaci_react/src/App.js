77786576c56eb590305ed8ec8cfd9c252a94457f
PredragTasic99 <predrag.tasic99@gmail.com>
2022-02-03 20:15:54 +0100

    ubacen useState i dugmici za complete i delete

--- domaci_react/src/App.js
+++ domaci_react/src/App.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import WishForm from "./components/WishForm";
 import WishList from "./components/WishList";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [wishes, setWishes] = useState([]);
 
   return (
     <div className="App">
       <header>
         <h1>Predrag's New year's bucket list</h1>
       </header>
-      <WishForm />
-      <WishList/>
+      <WishForm 
+        input={input}
+        wishes={wishes} 
+        setWishes={setWishes} 
+        setInput={setInput} 
+      />
+      <WishList
+        wishes={wishes}
+        setWishes={setWishes}
+      />
     </div>
   );
 }
